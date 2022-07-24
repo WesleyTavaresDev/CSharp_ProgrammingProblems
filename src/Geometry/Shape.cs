4993afc37f70f443219994a7f9484455d8f5cf28
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-07-24 10:35:02 -0300

    Geometry class renamed -> Shape class

---
+++ src/Geometry/Shape.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Shape
+{
+    void DrawShape(int width, int height)
+    {
+        Console.WriteLine("Please enter a number: ");
+
+        int number  = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+                Console.Write(number);
+                    
+            Console.Write('\n');
+        }
+    }
+}
