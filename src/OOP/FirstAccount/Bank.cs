da0d67993fbba52567a4f294bfd4d6ce5d118e35
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-07-24 16:37:04 -0300

    Template bank code added

--- src/OOP/FirstAccount/Bank.cs
+++ src/OOP/FirstAccount/Bank.cs
@@ -2,6 +2,23 @@ namespace FirstAccount
 {
     class Bank
     {
-        
+        static void Main()
+        {
+            Account heikkisAccount = new Account("Heikki's account", 100.00);
+            Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00);
+
+            Console.WriteLine("Intial state");
+            Console.WriteLine(heikkisAccount);
+            Console.WriteLine(heikkisSwissAccount);
+
+            heikkisAccount.Withdrawal(20);
+            Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.balance);
+            heikkisSwissAccount.Deposit(200);
+            Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.balance);
+
+            Console.WriteLine("End state");
+            Console.WriteLine(heikkisAccount);
+            Console.WriteLine(heikkisSwissAccount);
+        }
     }
 }
