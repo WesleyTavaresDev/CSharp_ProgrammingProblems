002b929d4803ac83f48a7f3c99647aadfb8664f1
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2022-07-24 11:54:11 -0300

    Rectangle exercise finished

--- src/Geometry/Geometry.cs
+++ src/Geometry/Geometry.cs
@@ -2,8 +2,12 @@ public class Geometry
 {
     static void Main()
     {
+        Console.WriteLine("----- Square -----");
         Square square = new Square();
-
         square.DrawSquare();
+
+        Console.WriteLine("----- Rectangle -----");
+        Rectangle rectangle = new Rectangle();
+        rectangle.DrawRectangle();
     }
 }
--- src/Geometry/Rectangle.cs
+++ src/Geometry/Rectangle.cs
@@ -1,4 +1,7 @@
 class Rectangle: Shape
 {
-    
+    public void DrawRectangle()
+    {
+        DrawShape(3, 5);
+    }    
 }
